22a1c8f974d00659760fc38bc7fa977f199d4413
Dan Connolly <connolly@agoric.com>
2022-03-15 11:28:21 -0500

    refactor(run-protocol): factor applyDelta etc. out of vault.js

--- packages/run-protocol/src/vaultFactory/vault.js
+++ packages/run-protocol/src/vaultFactory/vault.js
@@ -17,6 +17,7 @@ import { Far } from '@endo/marshal';
 import { makeTracer } from '../makeTracer.js';
 import { calculateCurrentDebt, reverseInterest } from '../interest-math.js';
 import { makeVaultKit } from './vaultKit.js';
+import { applyDelta, assertOnlyKeys, transfer } from '../contractSupport.js';
 
 const { details: X, quote: q } = assert;
 
@@ -416,57 +417,6 @@ export const makeInnerVault = (
     assertCloseable();
     return zcf.makeInvitation(closeHook, 'CloseVault');
   };
-
-  // The proposal is not allowed to include any keys other than these,
-  // usually 'Collateral' and 'RUN'.
-  const assertOnlyKeys = (proposal, keys) => {
-    const onlyKeys = clause =>
-      Object.getOwnPropertyNames(clause).every(c => keys.includes(c));
-    assert(
-      onlyKeys(proposal.give),
-      X`extraneous terms in give: ${proposal.give}`,
-    );
-    assert(
-      onlyKeys(proposal.want),
-      X`extraneous terms in want: ${proposal.want}`,
-    );
-  };
-
-  /**
-   * Stage a transfer between `fromSeat` and `toSeat`, specified as the delta between
-   * the gain and a loss on the `fromSeat`. The gain/loss are typically from the
-   * give/want respectively of a proposal. The `key` is the allocation keyword.
-   *
-   * @param {ZCFSeat} fromSeat
-   * @param {ZCFSeat} toSeat
-   * @param {Amount} fromLoses
-   * @param {Amount} fromGains
-   * @param {Keyword} key
-   */
-  const transfer = (fromSeat, toSeat, fromLoses, fromGains, key) => {
-    // Must check `isEmpty`; can't subtract `empty` from a missing allocation.
-    if (!AmountMath.isEmpty(fromLoses)) {
-      toSeat.incrementBy(fromSeat.decrementBy(harden({ [key]: fromLoses })));
-    }
-    if (!AmountMath.isEmpty(fromGains)) {
-      fromSeat.incrementBy(toSeat.decrementBy(harden({ [key]: fromGains })));
-    }
-  };
-
-  /**
-   * Apply a delta to the `base` Amount, where the delta is represented as
-   * an amount to gain and an amount to lose. Typically one of those will
-   * be empty because gain/loss comes from the give/want for a specific asset
-   * on a proposal. We use two Amounts because an Amount cannot represent
-   * a negative number (so we use a "loss" that will be subtracted).
-   *
-   * @param {Amount} base
-   * @param {Amount} gain
-   * @param {Amount} loss
-   * @returns {Amount}
-   */
-  const applyDelta = (base, gain, loss) =>
-    AmountMath.subtract(AmountMath.add(base, gain), loss);
 
   /**
    * Calculate the fee, the amount to mint and the resulting debt.
